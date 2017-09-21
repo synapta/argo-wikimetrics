0e54e6fd645816a4ce220437365eed706b9a82c2
Dario Salza <dsalza@hotmail.it>
2017-09-21 13:29:37 +0200

    M2 0.0.6 I
    Module 2 skeleton. Missing query+processing. Fails database connection

--- Module2/EntryPoint.js
+++ Module2/EntryPoint.js
@@ -1,6 +1,6 @@
 console.log("Loading Settings...")
 var fs = require('fs');
-var Client = require('mariasql');
+
 var HashMap=require('hashmap');
 var Module2=require('./Module2');
 var userProcessor = new HashMap();
@@ -9,14 +9,6 @@ fs.readFile('../../dbaccess.json', function (err, logData)
     if (err) throw err;
     var text = logData.toString();
     dbAccess=JSON.parse(text);
-    var wikiCaller = new Client
-    (
-        {
-            host: dbAccess.remote,
-            user: dbAccess.user,
-            password: dbAccess.password
-        }
-    );   
     fs.readFile('config.json', function (err, logData) 
     {
         if (err) throw err;
@@ -24,7 +16,7 @@ fs.readFile('../../dbaccess.json', function (err, logData)
         ConfigdataObj=JSON.parse(text);
         userProcessor.set(1,ConfigdataObj.filename);
         userProcessor.set(89765,ConfigdataObj.languages);
-        Module2.extract(userProcessor,wikiCaller,ConfigdataObj,function()
+        Module2.extract(userProcessor,dbAccess,ConfigdataObj,function()
         {
             console.log("Ended");
                 //estrai da userProcessor e salva gli utenti di interesse
--- Module2/Module2.js
+++ Module2/Module2.js
@@ -1,24 +1,30 @@
 const traductionLines=50;
 var userProcessor;
-var wikiCaller;
+var wikiCaller=null;
 var ConfigDataObj;
 var lineByLine;
 var langIndex;
 var liner;
-exports.extract=function(userProc,wikiCall,ConfigData,callbackEP)
+var callbackEP;
+var DBAccess;
+var Client;
+exports.extract=function(userProc,dBAccess,ConfigData,callbackEPx)
 {
     //STEP 1: open 1 language
     //STEP 2: read line and request traduction
     //STEP 3: request info from codes
     //STEP 4: save results
+    Client = require('mariasql');
     lineByLine = require('n-readlines');
     userProcessor=userProc;
-    wikiCaller=wikiCall;
+ 
     ConfigDataObj=ConfigData;
     langIndex=0;
-    LangLoop(callbackEP);
+    callbackEP=callbackEPx;
+    DBAccess=dBAccess;
+    LangLoop();
 }
-var LangLoop=function(callbackEP)
+var LangLoop=function()
 {    
     if(ConfigDataObj.languages.length>langIndex)
     {
@@ -26,33 +32,51 @@ var LangLoop=function(callbackEP)
         fin=finbase+(ConfigDataObj.languages[langIndex].value+".csv");
         liner = new lineByLine(fin);
         console.log(fin);
-        LangExecute(callbackEP);
+        var dbstring=ConfigDataObj.languages[langIndex].value.toLowerCase()+"wiki_p";
+        console.log("Connecting to DBstring..."+dbstring);
+        if(langIndex>0)
+            wikiCaller.end();
+        var clientopts=
+        {
+            host: DBAccess.host,
+            user: DBAccess.user,
+            password:  DBAccess.password,
+            port: DBAccess.port,
+            db: dbstring
+        };
+        console.log(clientopts);
+        wikiCaller=new Client(clientopts);
+        console.log("Connected(?!)");
+        LangExecute();
     }
     else
+    {
         callbackEP();
+        wikiCaller.end();
+    }
+      
 }
-var LangExecute=function(callbackEP)
+var LCallback=function()
 {
-    TraductionLoop(callbackEP,
-        function(callbackEP)
-        {
-            langIndex+=1;
-            LangLoop(callbackEP);
-        }
-    )
+    langIndex+=1;
+    LangLoop();
 }
-var TraductionLoop=function(callbackEP,LCallback)
+var LangExecute=function()
+{
+    TraductionLoop();
+}
+var TraductionLoop=function()
 {
     var line;
     if(line=liner.next())
-        TraductionExecute(line,callbackEP,LCallback)
+        TraductionExecute(line)
     else
     {
-        LCallback(callbackEP);
+        LCallback();
     }   
 
 }
-var TraductionExecute=function(line,callbackEP,LCallback)
+var TraductionExecute=function(line)
 {
     var RQ="";
     var i=0;
@@ -60,18 +84,52 @@ var TraductionExecute=function(line,callbackEP,LCallback)
     {
         if(i!=0)
             RQ+=",";
-        RQ+=("\""+decodeURI(line.toString('ascii')+"\""));
+        let lineascii=line.toString('ascii');
+        lineascii=lineascii.substring(0,lineascii.length-1)
+        RQ+=("\""+decodeURI(lineascii.split("wiki/")[1]+"\""));
         i++;
-    }while(i<traductionLines&&(line=liner.next()));
-    Query(RQ,callbackEP,LCallback,
-        function(callbackEP,LCallback)
+    } while(i<traductionLines&&(line=liner.next()));
+    var query1="select page_id from page where page_title in (\"Ginevra\") and page_namespace=0";
+    wikiCaller.query(query1, 
+        function(err, rows) 
         {
-            TraductionLoop(callbackEP,LCallback);
+            if (err)
+                throw err;
+            console.log(rows);
+            wikiCaller.end();
+            //UserTranslateExecution(rows);
+            throw new Error();
         }
-    )
+    );
+    /*Query(RQ,
+        function()
+        {
+            TraductionLoop();
+        }
+    )*/
 }
-var Query=function(RQ,callbackEP,LCallback,callbackTE)
+var UserTranslateExecution=function(rows)
+{
+    //monta la query numero due
+    var query2="";
+    //eseguila
+    wikiCaller.query(query2, 
+    function(err, rows) 
+    {
+        if (err)
+            throw err;
+        console.log(rows);
+        InsertUsersInProcessor(rows);
+    }
+);
+}
+var InsertUsersInProcessor=function(rows)
+{
+    //calcola punteggi e inserisci utenti nella HT
+    TraductionLoop();
+}
+var Query=function(RQ,callbackTE)
 {
     console.log(RQ);
-    callbackTE(callbackEP,LCallback);
+    callbackTE();
 }
